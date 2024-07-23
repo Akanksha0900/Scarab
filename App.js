adf3b49fa723b638e366815063a04370042343ae
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2024-07-23 18:07:54 +0530

    Updated Getting Started Page, added image, removed unused code from app.js

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 import * as Font from "expo-font";
 // Components to import
-import CustomButton from "./components/CustomButton";
 import { assets } from "./react-native.config";
 import GettingStarted from "./pages/GettingStarted";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Hello");
-  };
-
   return (
     <PaperProvider>
       <GettingStarted />
