d5f40e95681954b2eb620f92131258d292824134
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2024-07-24 21:03:33 +0530

    Improved Feed, Modified App.js

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
 import * as Font from "expo-font";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import SavedMessages from "./pages/SavedMessagesScreen";
 
 // Components to import
 import CustomButton from "./components/CustomButton";
 import GettingStarted from "./pages/GettingStarted";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Feed from "./pages/Feed";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,6 +23,7 @@ export default function App() {
           <Stack.Screen name="Getting Started" component={GettingStarted} />
           <Stack.Screen name="Signup" component={Signup} />
           <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Feed" component={Feed} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
